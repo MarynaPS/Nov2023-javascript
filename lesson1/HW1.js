082b837ef88d3997dd075e2bc1bc7fc94694d681
MarynaPS <pmaryna.s@gmail.com>
2023-11-22 09:15:51 +0100

    HW1.js

---
+++ lesson1/HW1.js
@@ -0,0 +1,58 @@
+
+
+//Додаткове для тих хто цікавився prompt`oм
+//За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+let n1='hello';
+console.log(n1);
+
+let n2='own';
+console.log(n2);
+
+let n3='com';
+console.log(n3);
+
+let n4='ua';
+console.log(n4);
+
+let a1=1;
+console.log(a1);
+
+let a2=10;
+console.log(a2);
+
+let a3=-999;
+console.log(a3);
+
+let a4=123;
+console.log(a4);
+
+let a5=3.14;
+console.log(a5);
+
+let a6=2.7;
+console.log(a6);
+
+let a7=16;
+console.log(a7);
+
+let b1=true;
+console.log(b1);
+
+let b2=false;
+console.log(b2);
+
+let firstName='Maryna';
+let middleName='Sergeevna'
+let lastName='Popova';
+let person='';
+console.log(person=`${firstName} ${middleName} ${lastName}`);
+
+let a=100;
+console.log(typeof a);
+
+let b='100';
+console.log(typeof b);
+
+let c=true;
+console.log(typeof c);
---
+++ lesson1/main.js
@@ -0,0 +1,33 @@
+console.log("123456");
+let n1=123;
+let s1='hello okten'
+let b1 = 5>6;
+console.log(b1);
+
+let b2=5!==6;
+console.log(b2);
+
+// let bool= 100 === '100';
+
+let x=null;
+console.log(typeof x);
+
+let a=10;
+let b=20;
+let c=a+b;
+
+console.log(c);
+console.log(a+b);
+
+let str1='hello';
+let str2='okten';
+let space=' ';
+console.log(str1+space+str2);
+console.log(`${str1} ${str2} !!!`);
+
+
+
+
+
+
+
